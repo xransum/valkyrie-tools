6b78b3add340bc76d26cc20b1e22d22634f40501
xransum <xransum@users.noreply.github.com>
2023-10-26 18:58:51 -0400

    Added valkyrie cli cmd, Updated package init & tests

--- src/valkyrie_tools/__init__.py
+++ src/valkyrie_tools/__init__.py
@@ -1,1 +1,10 @@
 """Valkyrie Tools."""
+
+__all__ = ["__version__", "DEFAULT_CONFIG_FILE", "config"]
+
+__version__ = "0.1.0"  # noqa
+
+# from .configs import Config
+
+# DEFAULT_CONFIG_FILE = os.path.join(os.path.expanduser("~"), ".valkyrie")
+# config = Config(DEFAULT_CONFIG_FILE)
